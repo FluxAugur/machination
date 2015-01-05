17b7bfa78e0646d698c8d2d71c595c2aa745d03c
Nathanial L. McConnell <nate@synaptian.com>
2015-01-04 22:09:29 -0500

    Feature: Preferences method to set Airport join mode when preferred networks are available.

--- preferences/airport.sh
+++ preferences/airport.sh
@@ -7,6 +7,20 @@ path_to_airport_binary="/System/Library/PrivateFrameworks/Apple80211.framework/V
 function osx_airport_disconnect_on_logout {
   local enabled; if [[ "$1" == "enabled" ]]; then enabled="YES"; else enabled="NO"; fi
 
+  echo "PREFERENCE: Airport - Toggle \"disconnect on logout\" enabled is ${enabled}."
+  shift 1;
+
   sudo $path_to_airport_binary prefs DisconnectOnLogout=${enabled}
 }
 
+# Set what to do when preferred networks are available
+#
+# Modes: Automatic | Preferred | Ranked | Recent | Strongest
+function osx_airport_join_mode {
+  local mode="$1";
+
+  echo "PREFERENCE: Airport - Join when preferred networks are available mode is ${mode} ($1)."
+  shift 1;
+
+  sudo $path_to_airport_binary JoinMode=${mode}
+}
