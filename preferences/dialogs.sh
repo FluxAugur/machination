0e08669d8f62b3fe11b62f931b6e2e1f5887cd9f
Nathanial L. McConnell <nate@synaptian.com>
2015-01-05 13:26:00 -0500

    Feature: Preference method to set file Dialog view mode.

--- preferences/dialogs.sh
+++ preferences/dialogs.sh
@@ -10,3 +10,26 @@ function osx_dialogs_auto_expanding_print_dialog {
   defaults write NSGlobalDomain PMPrintExpandedStateForPrint -bool $enabled
   defaults write NSGlobalDomain PMPrintExpandedStateForPrint2 -bool $enabled
 }
+
+# Set file dialog view mode
+function osx_dialogs_file_view_mode {
+  local mode="$1";
+
+  case $mode in
+  icon_grid)
+    mode="1";;
+  list)
+    mode="2";;
+  *)
+    mode="$mode";;
+  esac
+
+  echo "  + PREFERENCE: Desktop - File dialog mode is ${mode} ($1)."
+
+  defaults write NSGlobalDomain NavPanelFileListModeForOpenMode -int $mode
+  defaults write NSGlobalDomain NavPanelFileListModeForSaveMode -int $mode
+  defaults write NSGlobalDomain NSNavPanelFileListModeForOpenMode2 -int $mode
+  defaults write NSGlobalDomain NSNavPanelFileListModeForSaveMode2 -int $mode
+  defaults write NSGlobalDomain NSNavPanelFileLastListModeForOpenModeKey -int $mode
+  defaults write NSGlobalDomain NSNavPanelFileLastListModeForSaveModeKey -int $mode
+}
