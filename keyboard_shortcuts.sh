cd9375fa3a7073b7e57ff64768f03211aef82cda
Nathanial L. McConnell <nate@synaptian.com>
2015-01-04 23:10:52 -0500

    Feature: Keyboard Shortcuts method to toggle keyboard shortcuts in OS X.

---
+++ keyboard_shortcuts.sh
@@ -0,0 +1,12 @@
+# Keyboard Shortcuts #
+######################
+
+# Toggle keyboard shortcuts in OS X
+function osx_keyboard_shortcut_toggle {
+  local keyboard_shortcut_id="$1"; shift 1;
+  local enabled="$1"; shift 1;
+  local plist_path="/Users/$(whoami)/Library/Preferences/com.apple.symbolichotkeys.plist"
+
+  $path_to_plistbuddy -c "Delete :AppleSymbolicHotKeys:$keyboard_shortcut_id:enabled" $plist_path 2> /dev/null
+  $path_to_plistbuddy -c "Add :AppleSymbolicHotKeys:$keyboard_shortcut_id:enabled bool $enabled" $plist_path 2> /dev/null
+}
