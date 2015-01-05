f7f5afb3ee415adaa44c919469e0f366e525a8ce
Nathanial L. McConnell <nate@synaptian.com>
2015-01-05 13:09:08 -0500

    Feature: Preferences method to toggle whather (and which) drives are show on Desktop.

--- preferences/desktop.sh
+++ preferences/desktop.sh
@@ -9,3 +9,22 @@ function osx_desktop {
 
   defaults write com.apple.finder CreateDesktop -bool $enabled;
 }
+
+# Show internal drives on the Desktop
+function osx_desktop_displayed_items {
+  local key="$1"; shift 1;
+  local enabled; if [[ "$1" == "enabled" ]]; then enabled="true"; else enabled="false"; fi; shift 1;
+
+  case "$key" in
+  internal_hard_drives)
+    key="ShowHardDrivesOnDesktop";;
+  external_hard_drives)
+    key="ShowExternalHardDrivesOnDesktop";;
+  mounted_servers)
+    key="ShowMountedServersOnDesktop";;
+  removable_media)
+    key="ShowRemovableMediaOnDesktop";;
+  esac
+
+  defaults write com.apple.finder $key -bool $enabled;
+}
