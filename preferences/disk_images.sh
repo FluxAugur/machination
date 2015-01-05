2b0484967b42205db3d5757bb40d3eb1487fab8e
Nathanial L. McConnell <nate@synaptian.com>
2015-01-05 13:35:51 -0500

    Feature: Preferences method to toggle whether Disk Images are verified.

--- preferences/disk_images.sh
+++ preferences/disk_images.sh
@@ -11,3 +11,15 @@ function osx_disk_images_auto_mount {
   defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool $enabled
   defaults write com.apple.frameworks.finder OpenWindowForNewRemovableDisk -bool $enabled
 }
+
+# Toggle whether Disk Images are verified
+function osx_disk_images_verification {
+  local enabled; if [[ "$1" == "enabled" ]]; then enabled="true"; else enabled="false"; fi
+
+  echo "  + PREFERENCE: Disk Images - Toggle Disk Images are verified enabled is ${enabled} ($1)."
+
+  defaults write com.apple.frameworks.diskimages skip-idme -bool $enabled
+  defaults write com.apple.frameworks.diskimages skip-verify -bool $enabled
+  defaults write com.apple.frameworks.diskimages skip-verify-locked -bool $enabled
+  defaults write com.apple.frameworks.diskimages skip-verify-remote -bool $enabled
+}
