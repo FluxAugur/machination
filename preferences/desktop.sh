47c435b6bb31809202e8f0a88d74328ac83fa84f
Nathanial L. McConnell <nate@synaptian.com>
2015-01-05 13:15:14 -0500

    Feature: Preferences method to set icon size and grid spacing on Desktop.

--- preferences/desktop.sh
+++ preferences/desktop.sh
@@ -26,5 +26,23 @@ function osx_desktop_displayed_items {
     key="ShowRemovableMediaOnDesktop";;
   esac
 
+  echo "  + PREFERENCE: Desktop - Drives shown on Desktop enabled is ${enabled} ($key)."
+
   defaults write com.apple.finder $key -bool $enabled;
+}
+
+# Set Desktop icon size
+function osx_desktop_icon_size {
+  local size="$1"; shift 1;
+  local grid_spacing="$1"; shift 1;
+
+  echo "  + PREFERENCE: Desktop - Desktop icon size is ${size} ($grid_spacing)."
+
+  defaults write com.apple.finder DesktopViewOptions.IconSize -int $size
+
+  $path_to_plistbuddy -c "Delete :DesktopViewSettings:IconViewSettings:iconSize" /Users/${whoami}/Library/Preferences/com.apple.finder.plist 2> /dev/null
+  $path_to_plistbuddy -c "Add :DesktopViewSettings:IconViewSettings:iconSize integer $size" /Users/${whoami}/Library/Preferences/com.apple.finder.plist 2> /dev/null
+
+  $path_to_plistbuddy -c "Delete :DesktopViewSettings:IconViewSettings:gridSpacing" /Users/${whoami}/Library/Preferences/com.apple.finder.plist 2> /dev/null
+  $path_to_plistbuddy -c "Add : DesktopViewSettings:IconViewSettings:gridSpacing integer $grid_spacing" /Users/${whoami}/Library/Preferences/com.apple.finder.plist 2> /dev/null
 }
