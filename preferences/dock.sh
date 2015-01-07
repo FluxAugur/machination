ae452d4ea2de7b6173cdddea58fbe8711a7ca924
Nathanial L. McConnell <nate@synaptian.com>
2015-01-07 13:13:34 -0500

    Feature: Preferences method to toggle whether indicator lights are show under running applications in Dock.

--- preferences/dock.sh
+++ preferences/dock.sh
@@ -1,11 +1,20 @@
 # Dock #
 ########
 
-# Toggle whether hidden apps are dimmed in Dock
+# Toggle whether hidden applications are dimmed in Dock
 function osx_dock_hidden_app_dimming {
   local enabled; if [[ "$1" == "enabled" ]]; then enabled="true"; else enabled="false"; fi
 
-  echo "  + PREFERENCE: Dock - Toggle whether hidden apps are dimmed in Dock enabled is ${enabled} ($1)."
+  echo "  + PREFERENCE: Dock - Toggle whether hidden applications are dimmed in Dock enabled is ${enabled} ($1)."
 
   defaults write com.apple.dock showhidden -bool $enabled
+}
+
+# Toggle whether indicator lights are shown under running applications in Dock
+function osx_dock_indicator_lights {
+  local enabled; if [[ "$1" == "enabled" ]]; then enabled="true"; else enabled="false"; fi
+
+  echo "  + PREFERENCE: Dock - Toggle whether indicator lights are shown under running applications in Dock enabled is ${enabled} ($1)."
+
+  defaults write com.apple.dock show-process-indicators -bool $enabled
 }
