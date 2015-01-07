d799791a716e3c57bc9a62fea5140042c3cb4c02
Nathanial L. McConnell <nate@synaptian.com>
2015-01-07 12:48:55 -0500

    Feature: Preferences method to toggle hidden partitions visibility in Disk Utility.

--- preferences/disk_utility.sh
+++ preferences/disk_utility.sh
@@ -9,3 +9,12 @@ function osx_disk_utility_advanced_image_options {
 
   defaults write com.apple.DiskUtility advanced-image-options -int $enabled
 }
+
+# Toggle the visibility of hidden partititions in Disk Utility
+function osx_disk_utility_hidden_partitions {
+  local enabled; if [[ "$1" == "enabled" ]]; then enabled="1"; else enabled="0"; fi
+
+  echo "  + PREFERENCE: Disk Utility - Toggle Disk Utility hidden partititions visibility enabled is ${enabled} ($1)."
+
+  defaults write com.apple.DiskUtility DUShowEveryPartition -int $enabled
+}
